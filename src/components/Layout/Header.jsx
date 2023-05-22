450f3f9aace93e1728767a384782f6d1625d90dc
Szpytma <szpytmapawel@hotmail.com>
2023-05-22 15:57:53 +0100

    svg btn implemented

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,13 @@
 import "./Header.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
   return (
     <>
       <header className="header">
         <h1>React-Eats</h1>
-        <button>Cart</button>
+        <HeaderCartButton>Cart</HeaderCartButton>
       </header>
       <div className="main-image">
         <img src={mealsImage} alt="a table full of delicious food!"></img>
