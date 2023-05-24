e1490cb07e371507d03ad5daa577f593e5b1c7d8
Szpytma <szpytmapawel@hotmail.com>
2023-05-24 12:56:44 +0100

    add remove items implemented in cart

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -12,6 +12,8 @@ const cartReducer = (state, action) => {
       (item) => item.id === action.item.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
     let updatedItems;
 
     if (existingCartItem) {
@@ -24,12 +26,30 @@ const cartReducer = (state, action) => {
     } else {
       updatedItems = [...state.items, action.item];
     }
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
-  //   else if (action.type === "REMOVE") {
-  //   }
   return defaultCartState;
 };
 
