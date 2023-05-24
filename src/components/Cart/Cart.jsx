3eb51099aacc724971860b2eaf2f0205e4acd06c
Szpytma <szpytmapawel@hotmail.com>
2023-05-24 10:59:16 +0100

    items in a cart are now visibale

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,28 @@
+import { useContext } from "react";
 import Modal from "../UI/Modal";
 import "./Cart.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
+
 const Cart = ({ onClose }) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className="cart-items">
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li key={item.id}>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -13,13 +31,13 @@ const Cart = ({ onClose }) => {
       {cartItems}
       <div className="total">
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className="actions">
         <button className="button--alt" onClick={onClose}>
           Close
         </button>
-        <button className="button">Order</button>
+        {hasItems && <button className="button">Order</button>}
       </div>
     </Modal>
   );
